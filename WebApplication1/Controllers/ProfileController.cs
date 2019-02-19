78ce7af25089dc9c626fd1ce1b3595b45ad45915
Aykut Karagöz <aykutkaragoz94@gmail.com>
2019-02-19 14:51:08 +0300

    PartialProfile

---
+++ WebApplication1/Controllers/ProfileController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Web.Models.IdentityEntities;
+using static Web.BLL.Identity.MemberShipTools;
+
+namespace WebApplication1.Controllers
+{
+    public class ProfileController : Controller
+    {
+        // GET: Profile
+        public ActionResult ProfileIndex()
+        {
+            var id = HttpContext.GetOwinContext().Authentication.User.Identity.GetUserId();
+
+            User user = NewUserManager().FindById(id);
+
+            return PartialView("Partials/_PartialProfile",user);
+        }
+    }
+}
